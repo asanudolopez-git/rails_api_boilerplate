8689271283ba5e4787724b97cfbc1f57419bae48
Alex Sanudo <alexsanudo@alexs-mbp.home>
2025-01-04 19:55:46 -0500

    serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+
     def show
         render json: User.find(params[:id]), serializer: UserSerializer
+    end
+
+    def index
+        render json: User.all, each_serializer: UserSerializer
+    end
+    
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
     end
 end
