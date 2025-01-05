a6f2e43a10f0bd55b24571930a7690fb8de79759
Alex Sanudo <alexsanudo@alexs-mbp.home>
2025-01-04 20:14:19 -0500

    users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,35 @@
 class UsersController < ApplicationController
 
     def show
-        render json: User.find(params[:id]), serializer: UserSerializer
+        @user = User.find(params[:id])
+        render json: @user, serializer: UserSerializer
     end
 
     def index
-        render json: User.all, each_serializer: UserSerializer
+        @users = User.all
+        render json: @users, each_serializer: UserSerializer
     end
     
     def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user, serializer: UserSerializer
+        else
+            render json: { errors: @user.errors }, status: 422
+        end
     end
 
     def update
     end
 
     def destroy
+    end
+
+    private
+
+    def user_params
+        params.expect(
+            user: [:username, :email, :password]
+        )
     end
 end
